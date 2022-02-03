e0762ab7d2854238425c8e1c64947bdf854f1dcd
Alvin Indra <alvinindrap@gmail.com>
2022-02-03 20:53:53 +0700

    style: improve ui style

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,12 @@ function App() {
   return (
     <>
       <Header />
-      <div className="max-w-screen-lg mx-auto px-4 lg:px-0">
+      <div className="max-w-screen-xl mx-auto px-4">
         <CaseInIndonesia />
       </div>
+      <footer className='text-center py-3'>
+        Created By <a target="_blank" href="https://github.com/alvinindra/covid19-indonesia">Alvin Indra Pratama</a>
+      </footer>
     </>
   )
 }
--- src/components/CaseInIndonesia.jsx
+++ src/components/CaseInIndonesia.jsx
@@ -1,73 +1,96 @@
 import {useEffect, useState} from 'react'
 import $axios from '../utils/api'
+import iconPositif from '../assets/icon-positif.svg'
+import iconSembuh from '../assets/icon-sembuh.svg'
+import iconMeninggal from '../assets/icon-meninggal.svg'
 
 const CaseInIndonesia = () => {
-  const [dataIndonesia, setDataIndonesia] = useState(0)
+  const [dataIndonesia, setDataIndonesia] = useState('')
 
   useEffect(() => {
-    setTimeout($axios.get('/countries/id').then(res => {
+    $axios.get('/countries/id').then(res => {
       setDataIndonesia(res.data)
     }).catch(error => {
       console.error(error)
-    }))
-  }, [dataIndonesia])
+    })
+  }, [])
 
-  const { todayCases, todayRecovered, todayDeaths, cases, recovered, deaths } = dataIndonesia
+  const { todayCases, todayRecovered, todayDeaths, cases, recovered, deaths, updated } = dataIndonesia
+
+  function timeConverter(UNIX_timestamp){
+    const dateObject = new Date(UNIX_timestamp)
+    const dayName = dateObject.toLocaleString("id-ID", {weekday: 'long'})
+    const day = dateObject.toLocaleString("id-ID", {day: 'numeric'})
+    const month = dateObject.toLocaleString("id-ID", {month: 'long'})
+    const year = dateObject.toLocaleString("id-ID", {year: 'numeric'})
+    return `${dayName}, ${day} ${month} ${year}`
+  }
 
   return (
     <div>
-      <div className='mb-8'>
-        <div className='text-2xl font-bold mb-4'>Jumlah Kasus Harian :</div>
+      <div className='mb-[50px]'>
+        <div className='text-lg sm:text-2xl font-bold mb-1'>Kasus Harian :</div>
         { dataIndonesia &&
           <>
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-y-3 md:gap-5'>
-              <div className='rounded-md p-3 bg-gradient-to-r from-red-600 to-rose-500 shadow-md'>
-                <div className='text-lg font-medium text-white'>Terkonfirmasi :</div>
-                <div className='text-md text-white'>
+            <div className='text-sm text-[#3B3B3B] mb-3'>
+              Update terkini : {timeConverter(updated)}
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-5'>
+              <div className='rounded p-4 bg-white text-center shadow-custom'>
+                <img className='w-26px h-26px mb-3 mx-auto' src={iconPositif} alt="" />
+                <div className='text-2xl text-[#8068F4] font-bold'>
                   {todayCases.toLocaleString()}
                 </div>
+                <div className='text-base'>Positif</div>
               </div>
-              <div className='rounded-md p-3 bg-gradient-to-r from-green-600 to-emerald-400  shadow-md'>
-                <div className='text-lg font-medium text-white'>Sembuh :</div>
-                <div className='text-md text-white'>
+              <div className='rounded p-4 bg-white text-center shadow-custom'>
+                <img className='w-26px h-26px mb-3 mx-auto' src={iconSembuh} alt="" />
+                <div className='text-2xl text-[#2ECC71] font-bold'>
                   {todayRecovered.toLocaleString()}
                 </div>
+                <div className='text-base'>Sembuh</div>
               </div>
-              <div className='rounded-md p-3 bg-gradient-to-r from-slate-700 to-slate-500 shadow-md'>
-                <div className='text-lg font-medium text-white'>Meninggal :</div>
-                <div className='text-md text-white'>
+              <div className='rounded p-4 bg-white text-center shadow-custom'>
+                <img className='w-26px h-26px mb-3 mx-auto' src={iconMeninggal} alt="" />
+                <div className='text-2xl text-[#FF4532] font-bold'>
                   {todayDeaths.toLocaleString()}
                 </div>
+                <div className='text-base'>Meninggal</div>
               </div>
             </div>
           </>
         }
       </div>
-      <div className='text-2xl font-bold mb-4'>Jumlah Total Kasus :</div>
-      { dataIndonesia &&
-        <>
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-y-3 md:gap-5'>
-            <div className='rounded-md p-3 bg-gradient-to-r from-red-600 to-rose-500 shadow-md'>
-              <div className='text-lg font-medium text-white'>Terkonfirmasi :</div>
-              <div className='text-md text-white'>
-                {cases.toLocaleString()}
+      <div className="mb-[50px]">
+        <div className='text-lg sm:text-2xl font-bold mb-4'>Jumlah Kasus di Indonesia Saat Ini :</div>
+        { dataIndonesia &&
+          <>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-5'>
+                <div className='rounded p-4 bg-white text-center shadow-custom'>
+                  <img className='w-26px h-26px mb-3 mx-auto' src={iconPositif} alt="" />
+                  <div className='text-2xl text-[#8068F4] font-bold'>
+                    {cases.toLocaleString()}
+                  </div>
+                  <div className='text-base'>Positif</div>
+                </div>
+                <div className='rounded p-4 bg-white text-center shadow-custom'>
+                  <img className='w-26px h-26px mb-3 mx-auto' src={iconSembuh} alt="" />
+                  <div className='text-2xl text-[#2ECC71] font-bold'>
+                    {recovered.toLocaleString()}
+                  </div>
+                  <div className='text-base'>Sembuh</div>
+                </div>
+                <div className='rounded p-4 bg-white text-center shadow-custom'>
+                  <img className='w-26px h-26px mb-3 mx-auto' src={iconMeninggal} alt="" />
+                  <div className='text-2xl text-[#FF4532] font-bold'>
+                    {deaths.toLocaleString()}
+                  </div>
+                  <div className='text-base'>Meninggal</div>
+                </div>
               </div>
-            </div>
-            <div className='rounded-md p-3 bg-gradient-to-r from-green-600 to-emerald-400  shadow-md'>
-              <div className='text-lg font-medium text-white'>Sembuh :</div>
-              <div className='text-md text-white'>
-                {recovered.toLocaleString()}
-              </div>
-            </div>
-            <div className='rounded-md p-3 bg-gradient-to-r from-slate-700 to-slate-500 shadow-md'>
-              <div className='text-lg font-medium text-white'>Meninggal :</div>
-              <div className='text-md text-white'>
-                {deaths.toLocaleString()}
-              </div>
-            </div>
-          </div>
-        </>
-      }
+          </>
+        }
+      </div>
     </div>
   )
 }
